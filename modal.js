f7e195b387e296952376e969aa3c11471438f128
Jason Kando <kando.jason@live.fr>
2021-08-02 16:46:15 +0200

    Label lié avec FOR et ID OK / 2 carractères min OK / Email Ok

--- modal.js
+++ modal.js
@@ -12,6 +12,8 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const close = document.querySelector('.close')
+const firstPrenom = document.getElementById("first")
+console.log(firstPrenom);
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -24,4 +26,13 @@ close.addEventListener('click', () => {
   console.log('yes');
   modalbg.style.display = "none"
   // alert('Êtes vous sur de vouloir quitter')
+})
+
+// Message d'erreur
+firstPrenom.addEventListener('keyup', (e) =>{
+  if (firstPrenom.validity.typeMismatch) {
+    firstPrenom.setCustomValidity(" N'est pas valide")
+  } else {
+    firstPrenom.setCustomValidity('')
+  }
 })
