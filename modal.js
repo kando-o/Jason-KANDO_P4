30a8ff7fbe8a8ee735568281302947d51579120b
Jason Kando <kando.jason@live.fr>
2021-08-02 14:45:23 +0200

    First Commit

---
+++ modal.js
@@ -0,0 +1,27 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const close = document.querySelector('.close')
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+// Close le BGround 
+close.addEventListener('click', () => {
+  console.log('yes');
+  modalbg.style.display = "none"
+  alert('Êtes vous sur de vouloir quitter')
+})
